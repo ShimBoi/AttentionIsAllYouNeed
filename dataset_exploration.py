b6437134d7d589f6d7f357e6a039b2a93762e4d5
shimboi <jshim1213@utexas.edu>
2025-09-26 22:20:29 -0500

    Added dataset exploration and sample data printing

---
+++ dataset_exploration.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+
+def pprint(title, data):
+    print(f"### {title} ###")
+    print(data)
+    print("-----------------")
+
+dataset = load_dataset("wmt14", "de-en")
+pprint("FULL DATASET OVERVIEW", dataset)
+
+# view some training samples
+sample = dataset["train"][1]
+de = sample["translation"]["de"]
+en = sample["translation"]["en"]
+pprint("SAMPLE DATA", f"ORIGINAL: \n\t{de}\n\nTRANSLATED: \n\t{en}")
